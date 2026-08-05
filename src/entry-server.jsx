309837b8f8540502980012d595804d2289e8543a
David01-glitch <b3solns1@gmail.com>
2026-08-05 19:11:58 +0530

    Deploy Ios 7256 static site (phone 1-888-527-7658, GA G-9JFBRX0KFJ)

--- src/entry-server.jsx
+++
@@ -1,6 +0,0 @@
-import { renderToString } from 'react-dom/server'
-import { StaticRouter } from 'react-router-dom/server'
-import App from './App.jsx'
-export function render(url) {
-  return renderToString(<StaticRouter location={url}><App /></StaticRouter>)
-}
